d6b1a0d6dade8e403a450621e2fdd7a71dc6d5bc
Scott Yoder <yodersb@gmail.com>
2016-09-10 14:35:21 -0400

    initial check-in

---
+++ main.js
@@ -0,0 +1,19 @@
+$( () => {
+  // alert("foo");
+  alex = new Image(600, 800);
+  alex.src = "alex.png";
+  $('#doll').append(alex);
+
+  $('.item').click( (e)=> {
+    var imgName = $(e.target).attr('data-img');
+    var existingImg = $('#doll img[src=\"'+imgName+'\"]');
+    if (existingImg.length > 0) {
+      existingImg.remove();
+    } else {
+      item = new Image(600, 800);
+      item.src = imgName;
+      $('#doll').append(item);
+    }
+  });
+});
+
